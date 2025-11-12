9328a380f779227b0d5ad29ec30fe9ff413eb470
MagicalTinCan <caelumcampbell@gmail.com>
2025-11-12 14:11:00 -0500

    Getting closer, editted upload/download networking capabilities. still incomplete

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include <cstdlib>
 #include "export.h"
 
-
 int main() {
     Export exportdat;
-    exportdat.importData();
+    //exportdat.importData();
     //system("help");
     //std::cout << "test" << std::endl;
+    exportdat.turn = 1;
+    std::cout << 1;
+    //exportdat.importData();
+    std::cout << 2;
     exportdat.exportData();
     return 0;
 }
