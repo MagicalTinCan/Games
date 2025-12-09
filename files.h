5f68f48f517a4ef66147697c0e11888a450c76c2
MagicalTinCan <caelumcampbell@gmail.com>
2025-12-09 16:03:08 -0500

    made the files.h work for any windows computer, not just mercer computers

--- files.h
+++ files.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <cstdlib>
 
 /*
 
@@ -13,8 +14,8 @@ This file was made entirely by Samuel Campbell
 struct files {
     std::string gitRepository = "https://github.com/MagicalTinCan/Server.git";
     std::string gitRemoteName = "noTouchie";
-    std::string localDirectory = "C:\\Users\\EGR126.03.CSCMERCER\\dataShtuff\\";
-    std::string projectDirectory = std::filesystem::current_path().string() + "\\";
+    std::string localDirectory = "C:\\Users\\" + (std::string)std::getenv("USERNAME") + "\\dataShtuff\\";
+    std::string projectDirectory = std::filesystem::path(__FILE__).parent_path().u8string() + "\\";
     std::string pythonInitalizer = projectDirectory + "initalizeData.py";
     std::string batchUploader = projectDirectory + "upload.bat";
     std::string batchPull = projectDirectory + "pull.bat";
